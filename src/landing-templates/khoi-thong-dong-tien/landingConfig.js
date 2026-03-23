a592f567c966001fd32e27adbd419b6530c7c5f5
MALI <dangpkzxy@gmail.com>
2026-03-24 00:04:25 +0700

    ok

--- src/landing-templates/khoi-thong-dong-tien/landingConfig.js
+++ src/landing-templates/khoi-thong-dong-tien/landingConfig.js
@@ -1,6 +1,6 @@
 // Edit this file when you need to update the event schedule or thank-you page link.
 export const KHOI_THONG_DONG_TIEN_CONFIG = {
-  eventStart: "2026-03-24T20:00:00+07:00",
-  ctaScheduleLabel: "20h00, 24-25-26-27/03",
-  zaloLink: "https://zalo.me/g/fedjcewig0zvk3kkvvp0",
+  eventStart: "2026-04-01T20:00:00+07:00",
+  ctaScheduleLabel: "20h00, 01-02-03-04/04",
+  zaloLink: "https://zalo.me/g/bncwc6wd9unffxh6ofqj",
 };
