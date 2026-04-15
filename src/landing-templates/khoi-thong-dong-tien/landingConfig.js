8dbd3f312a5b82fc2394c4e1d2d1415a53075d6a
MALI <dangpkzxy@gmail.com>
2026-04-15 11:47:22 +0700

    cập nhật khóa k43 ktdt

--- src/landing-templates/khoi-thong-dong-tien/landingConfig.js
+++ src/landing-templates/khoi-thong-dong-tien/landingConfig.js
@@ -1,6 +1,6 @@
 // Edit this file when you need to update the event schedule or thank-you page link.
 export const KHOI_THONG_DONG_TIEN_CONFIG = {
-  eventStart: "2026-04-07T20:00:00+07:00",
-  ctaScheduleLabel: "20h00, 07-08-09-10/04",
-  zaloLink: "https://zalo.me/g/xwccytnob7mszj7bvguh",
+  eventStart: "2026-04-25T20:00:00+07:00",
+  ctaScheduleLabel: "20h00, 25-26-27-28/04",
+  zaloLink: "https://zalo.me/g/fes8nosde6ehnawcjznz",
 };
