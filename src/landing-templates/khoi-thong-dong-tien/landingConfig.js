0688989556cde1959343e574c4e2f75ca535a70f
Nguyen Anh Dang <dangpkzxy@gmail.com>
2026-05-11 16:17:54 +0700

    cap nhat ngay

--- src/landing-templates/khoi-thong-dong-tien/landingConfig.js
+++ src/landing-templates/khoi-thong-dong-tien/landingConfig.js
@@ -5,7 +5,7 @@ export const KHOI_THONG_HERO_BANNER_URL =
   "https://s3-hn1-api.longvan.vn/video-khoa-hoc/videos/1777910467237-372116712-banner-optimized.jpg";
 
 export const KHOI_THONG_DONG_TIEN_CONFIG = {
-  eventStart: "2026-05-06T20:00:00+07:00",
-  ctaScheduleLabel: "20h00, 06-07-08-09/05",
-  zaloLink: "https://zalo.me/g/yxqb47xi4yinh4tubevq",
+  eventStart: "2026-05-21T20:00:00+07:00",
+  ctaScheduleLabel: "20h00, 21-22-23-24/05",
+  zaloLink: "https://zalo.me/g/nqn15yafhgiwrjembzxn",
 };
